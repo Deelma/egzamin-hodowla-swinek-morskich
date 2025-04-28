98a901de1cefd32d72d1d27c2187a89c0fa0b4d4
Deelma <156914350+Deelma@users.noreply.github.com>
2025-04-28 10:25:10 +0200

    Add files via upload

---
+++ peruwianka.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hodowla świnek morskich</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Hodowla świnek morskich - zamów świnkowe maluszki</h1>
+    </header>
+    <main>
+        <section id="lewo">
+            <section id="menu">
+                <a href="peruwianka.php">Rasa Peruwianka</a>
+                <a href="american.php">Rasa American</a>
+                <a href="crested.php">Rasa Crested</a>
+            </section>
+            <section id="glowny">
+                <img src="peruwianka.jpg" alt="Świnka morska rasy peruwianka">
+                <?php
+
+                    // ! SKRYPT 2
+
+
+                ?>
+                <hr>
+                <h2>Świnki w tym miocie</h2>
+                <?php
+
+                    // ! SKRYPT 3
+
+
+                ?>
+            </section>
+        </section>
+        <section id="prawo">
+            <h3>Poznaj wszystkie rasy świnek morskich</h3>
+            <ol>
+                <?php
+
+                    // ! Skrypt 1
+
+
+                ?>
+            </ol>
+        </section>
+    </main>
+        <footer>
+            <p>Strone wykonał: Nikodem Warmowski</p>
+        </footer>
+</body>
+</html>
